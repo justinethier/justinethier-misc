5690711037359869af635416ce3639f1446f2192
Justin Ethier <justin.ethier@gmail.com>
2017-09-22 19:53:54 -0400

    WIP

--- fastcgi/example.c
+++ fastcgi/example.c
@@ -3,10 +3,33 @@
 
 int main(void)
 {
+ FCGX_Request request;
 
- while(FCGI_Accept() >= 0)
+ FCGX_Init();
+ FCGX_InitRequest(&request, 0, 0);
+ //while(FCGI_Accept() >= 0)
+ while(FCGX_Accept_r(&request) >= 0)
  {
-  printf("Content-type: text/html\r\nStatus: 200 OK\r\n\r\n");
+  const char * uri = FCGX_GetParam("REQUEST_URI", request.envp);
+  //printf("Content-type: text/html\r\nStatus: 200 OK\r\n\r\n");
+  FCGX_FPrintF(request.out, "Content-type: text/html\r\nStatus: 200 OK\r\n\r\n");
+
+
+  FCGX_FPrintF(request.out, 
+        "\r\n"
+        "<html>\n"
+        "  <head>\n"
+        "    <title>Hello, World!</title>\n"
+        "  </head>\n"
+        "  <body>\n"
+        "    <h1>Hello from %s !</h1>\n"
+        "  </body>\n"
+        "</html>\n", uri);
+  
+  // TODO: see remaining example for reading content:
+  // http://chriswu.me/code/hello_world_fcgi/main_v2.cpp
+  // and: http://chriswu.me/blog/getting-request-uri-and-content-in-c-plus-plus-fcgi/
+  
 
  }
 
