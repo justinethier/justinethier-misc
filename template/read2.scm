7da7514ae7ca2899957b6999f540851020315a2d
Justin Ethier <justin.ethier@gmail.com>
2020-10-14 17:51:06 -0400

    WIP, refactoring code to read strings

--- template/read2.scm
+++ template/read2.scm
@@ -9,10 +9,35 @@
 ;   - yes, split string at that point, put beginning into expr list, parse the rest (possibly more reads req'd)
 ;   - no, append to string list
 ;
+
+TODO: want next 4 vars to be private, really
 (define *read-size* 2) ;; TODO: 1024?
-(define fp (open-input-file "view-4.html"))
+(define fp (open-input-file "view-2.html"))
 (define inp (read-string *read-size* fp))
 (define exprs '())
+
+(define (read-next-string!)
+  (set! inp (read-string *read-size* fp)))
+
+(define (append-next-string!)
+  (set! inp (string-append inp (read-string *read-size* fp)))
+  inp)
+
+(define (set-current-string! str)
+  (set! inp str)
+  inp)
+
+;; Return next char from input, starting at position `pos`.
+;; Will read more from input stream if necessary
+(define (next-char str pos)
+  (cond
+    ((< (+ 1 pos) (string-length str))
+     (string-ref str (+ pos 1)))
+    (else
+     (append-next-string!)
+     (next-char str 0)
+     )))
+
 
 ;; Return position of char `chr` within string `str`, starting from index `start`
 (define (string-pos str chr start)
@@ -25,71 +50,65 @@
      (else
        (loop (+ i 1))))))
 
-;; Return next char from input, starting at position `pos`.
-;; Will read more from input stream if necessary
-(define (next-char str pos)
-  (cond
-    ((< (+ 1 pos) (string-length str))
-     (string-ref str (+ pos 1)))
-    (else
-     (set! str (string-append str (read-string *read-size* fp)))
-     (next-char str 0)
-     )))
-
-;; Parse out data for a scheme template comment
-;; Basically reads and discards the whole comment.
-(define (parse-string-comment! str start)
-  (let loop ((s str)
-             (pos start))
-    (let ((i (string-pos s #\# pos)))
-(write `(loop ,i ,s))(newline)
-      (cond
-        (i
-         (let ((c (next-char s i)))
-(write `(DEBUG c ,c ,s ,pos))
-           (cond
-            ((eq? c #\})
-             ;; Start buffer from end of comment
-             (substring s (+ i 2) (string-length s)))
-            (else
-              (loop (read-string *read-size* fp) 0)) )))
-        (else
-         (loop (read-string *read-size* fp)
-               0))))))
-
-(define (parse-expr! str start)
-  (define expr "")
-  (define (add! str)
-    (set! expr (string-append expr str)))
-
-  (let loop ((s str)
-             (pos start))
-    (let ((i (string-pos s #\} pos)))
-(write `(loop ,i ,s))(newline)
-      (cond
-        (i
-         (let ((c (next-char s i)))
-(write `(DEBUG c ,c ,s ,pos))
-           (cond
-            ((eq? c #\})
-             ;; Return expression and remaining buffer
-             (if (> (- i 2) pos)
-                 (add! (substring s pos (string-length s))))
-             ;(values
-             ;  expr ;; Scheme expression (as string)
-               (substring s (+ i 2) (string-length s))) ;; Remaining buffer
-             ;)
-            (else
-              (add! (substring s pos (string-length s)))
-              (loop (read-string *read-size* fp) 0)) )))
-        (else
-         (add! (substring s pos (string-length s)))
-         (loop (read-string *read-size* fp)
-               0))))))
+;;; Parse out data for a scheme template comment
+;;; Basically reads and discards the whole comment.
+;(define (parse-string-comment! str start)
+;  (let loop ((s str) ;; TODO: don't store string locally
+;             (pos start))
+;    (let ((i (string-pos s #\# pos)))
+;(write `(loop ,i ,s))(newline)
+;      (cond
+;        (i
+;         (let ((c (next-char s i)))
+;(write `(DEBUG c ,c ,s ,pos)) (newline)
+;           (cond
+;            ((eq? c #\})
+;(write `(DEBUG ,s ,i)) (newline)
+;             ;; Start buffer from end of comment
+;             (substring s (+ i 2) (string-length s)))
+;            (else
+;              (loop (read-string *read-size* fp) 0)) )))
+;        (else
+;         (loop (read-string *read-size* fp)
+;               0))))))
+;
+;(define (parse-expr! str start)
+;  (define expr "")
+;  (define (add! str)
+;    (set! expr (string-append expr str)))
+;
+;  (let loop ((s str)
+;             (pos start))
+;    (let ((i (string-pos s #\} pos)))
+;(write `(loop ,i ,s))(newline)
+;      (cond
+;        (i
+;         (let ((c (next-char s i)))
+;(write `(DEBUG c ,c ,s ,pos)) (newline)
+;           (cond
+;            ((eq? c #\})
+;(write `(DEBUG ,s ,i)) (newline)
+;             ;; Return expression and remaining buffer
+;             (if (> (- i 2) pos)
+;                 (add! (substring s pos (string-length s))))
+;             ;(values
+;             ;  expr ;; Scheme expression (as string)
+;               (substring s (+ i 2) (string-length s))) ;; Remaining buffer
+;             ;)
+;            (else
+;              (add! (substring s pos (string-length s)))
+;              (loop (read-string *read-size* fp) 0)) )))
+;        (else
+;         (add! (substring s pos (string-length s)))
+;         (loop (read-string *read-size* fp)
+;               0))))))
 
 ;; Top-level parser
 (define (parse)
   (cond
+
+;; TODO: do not work with inp here! need to abstract that somehow
+
     ;; EOF?
     ((eof-object? inp)
      ;(terminate!)
@@ -110,19 +129,19 @@
             ;; TODO: check next char (may require another read)
             (let ((c (next-char inp 0)))
               (cond
-                ((eq? c #\#)
-                 (set! inp (parse-string-comment! inp 1))
-                 (parse))
-                ((eq? c #\{)
-                 (set! inp (parse-expr! inp 1))
-                 (parse))
+              ;  ((eq? c #\#)
+              ;   (set! inp (parse-string-comment! inp 1))
+              ;   (parse))
+              ;  ((eq? c #\{)
+              ;   (set! inp (parse-expr! inp 1))
+              ;   (parse))
                 (else
                   (write "TODO: parse scheme expression")
                   (newline)
                   (exit 1)))))  ;; TODO: (parse)
           (else
             (set! exprs (cons inp exprs))
-            (set! inp (read-string *read-size* fp))
+            (read-next-string!)
             (parse)))))))
 
 (parse)
