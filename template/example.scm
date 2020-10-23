c6c286fd1eda0f6644d1a28960c975c7726b1e12
Justin Ethier <justin.ethier@gmail.com>
2020-10-23 19:03:06 -0400

    Cleanup, added TODO's

--- template/example.scm
+++ template/example.scm
@@ -1,9 +1,18 @@
 (import (scheme base)
         (parser)
-        (template)
-        )
+        (template))
 
-;; TODO:
+;; TODO: 
+;; - enhanced cache to take file timestamp into account? see below
+;; - scheme expression and statement syntax
+;; - unit tests via test.scm, need to handle many edge cases
+;; - cyclone winds package
+;; - real example using lighthttpd and fastcgi
+;;   EG: https://github.com/jerryvig/lighttpd-fastcgi-c
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Older Notes:
 
 ;; Page record type:
 ;;  filename
@@ -20,13 +29,10 @@
 ;;    track file timestamp and reload if necessary,
 ;;    but this will be cyclone-only.
 ;;   Otherwise cache forever, I guess. Or time out if no access in X seconds
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; TODO: new module to encapsulate view 
 
-;; TODO: low-level render ??
-
-;; TODO: (define (add-arg!) ????
-
+;; Basic example:
 (define args '((row . (cons "view-1.html" "View 1"))
                (link . car)
                (desc . cdr)))
