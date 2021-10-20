21dc3b4d93b5ed6105e461ff835a48f617c043bf
Justin Ethier <justin.ethier@gmail.com>
2021-10-20 09:31:01 -0700

    Stub out test for key/value functionality

--- go/kv-store/lsb/lsb_test.go
+++ go/kv-store/lsb/lsb_test.go
@@ -42,3 +42,32 @@ func TestSeq(t *testing.T) {
     t.Error("Should never fail")
   }
 }
+
+func TestKeyValue(t *testing.T) {
+  ResetDB()
+
+  for i := 0; i < b.N; i++ {
+    // TODO: encode predictable value for i
+    token := make([]byte, 8)
+    rand.Read(token)
+    Set(strconv.Itoa(i), Value{Data: token, ContentType: "test content"})
+  }
+
+  // TODO: verify i contains expected value
+  //for i := 0; i < b.N; i++ {
+  //  m.Get(strconv.Itoa(i))
+  //}
+
+  for i := 0; i < b.N; i++ {
+    Delete(strconv.Itoa(i))
+  }
+
+  // TODO: verify key does not exist for i
+  //for i := 0; i < b.N; i++ {
+  //  m.Get(strconv.Itoa(i))
+  //}
+
+
+  // TODO: add a key back
+  // TODO verify that key exists now
+}
