4600f5df98e5edbf166af936e5e8a59dc744275a
Justin Ethier <justin.ethier@gmail.com>
2021-10-21 20:01:08 -0700

    WIP, sst functions

--- go/kv-store/lsb/sst.go
+++ go/kv-store/lsb/sst.go
@@ -2,6 +2,11 @@
 package lsb
 
 import (
+    "log"
+    "fmt"
+    "io/ioutil"
+    "regexp"
+    "strconv"
 )
 
 type SstBuf struct {
@@ -31,5 +36,47 @@ func (s *SstBuf) Set(k string, value interface{}, deleted bool) {
   }
 
   //TODO: (*s).Flush()
+}
+
+// TODO:
+//func (s *SstBuf) Flush() {
+//  if len((*s).Buffer) == 0 {
+//    return
+//  }
+//
+//  // TODO: remove duplicates from buffer
+//
+//
+//  // TODO: sort list
+//
+//  // TODO: flush buffer to disk
+//  var filename = (*s).NextSstFilename()
+//
+//  var buf []SstEntry
+//  (*s).buffer = buf
+//}
+
+func (s *SstBuf) NextSstFilename() string {
+  files, err := ioutil.ReadDir((*s).Path)
+  if err != nil {
+      log.Fatal(err)
+  }
+
+  var sstFiles []string
+  for _, file := range files {
+    matched, _ := regexp.Match(`^sorted-string-table-[0-9]*\.json`, []byte(file.Name()))
+    if matched && !file.IsDir() {
+      //fmt.Println(file.Name(), file.IsDir())
+      sstFiles = append(sstFiles, file.Name())
+    }
+  }
+
+  if len(sstFiles) > 0 {
+    var latest = sstFiles[len(sstFiles)-1][20:24]
+    n, _ := strconv.Atoi(latest)
+    return fmt.Sprintf("sorted-string-table-%04d.json", n + 1)
+  }
+
+  return "sorted-string-table-0000.json"
 }
 
