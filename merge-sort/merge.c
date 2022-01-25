3ed6f073d79589f0d74379ab10dd3c772dbf30a8
Justin Ethier <justin.ethier@gmail.com>
2022-01-24 20:05:07 -0800

    WIP

--- merge-sort/merge.c
+++ merge-sort/merge.c
@@ -9,8 +9,11 @@ void print_array(int *arr, int len) {
   printf("\n");
 }
 
+// Take an array where each half has been sorted, and sort
+// the whole array in place
 void merge_sorted(int *lis, int l, int m, int r) {
-  int *tmp = malloc(r - l + 1);
+  //int *tmp = malloc((r - l + 1) * sizeof(int));
+  int tmp[(r - l) + 1];
   int t = 0;
   
   int ll = l, mm = m;
@@ -27,21 +30,26 @@ void merge_sorted(int *lis, int l, int m, int r) {
   }
 
   print_array(tmp, r - l + 1);
-  memcpy(lis + l, tmp, (r - l) + 1);
-  free(tmp);
+  memcpy(lis + l, tmp, ((r - l) + 1) * sizeof(int));
+  //free(tmp);
   print_array(lis, r - l + 1);
 }
 
-void merge(int *lis, int l, int m, int r) {
-  // TODO: if
+void merge(int *lis, int l, int r) {
+  if (l < r) {
+    int m = l + (r - l) / 2;
 
-  merge(lis, l, (m-l)/2, m);
-  merge(lis, m, (r-m)/2, r);
-  merge_sorted(lis, l, m, r);
+    merge(lis, l, m);
+    merge(lis, m + 1, r);
+    merge_sorted(lis, l, m, r);
+  }
 }
 
 void main() {
   int test[] = {2, 4, 6, 1, 3, 5};
+  int test2[] = {5, 3, 4, 2, 6, 1, -1, 8};
   merge_sorted(test, 0, 3, 5);
   print_array(test, 6);
+  merge(test2, 0, 7);
+  print_array(test2, 8);
 }
