e1f987a671eef8713a2985a9c23f7ccb62dc2116
Justin Ethier <justin.ethier@gmail.com>
2022-01-25 15:17:25 -0800

    WIP

--- merge-sort/merge.c
+++ merge-sort/merge.c
@@ -30,31 +30,49 @@ void test_eq(int *a, int *b, int len) {
 
 // Take an array where each half has been sorted, and sort
 // the whole array in place
+//
+// lis - array to sort
+// l - start of left half of array
+// m - end of the left half of array
+// r - end of the right half
 void merge_sorted(int *lis, int l, int m, int r) {
-  int tmp[(r - l) + 1];
-  int t = 0;
-  
-  int ll = l, mm = m;
-  while(ll < m || mm <= r){
-    if (mm <= r && (ll == m || lis[ll] > lis[mm])) {
-      tmp[t] = lis[mm];
-      t++;
-      mm++;
-    } else {
-      tmp[t] = lis[ll];
-      t++;
-      ll++;
-    }
+  int len = (r - l) + 1;
+  int buf[len];
+  int i, j = 0, k = m + 1;
+
+TODO: only copy entries from l to r
+
+  for (i = 0; i < len; i++) { // Copy the array
+    buf[i] = lis[i];
   }
 
-  print_array(tmp, r - l + 1);
-  memcpy(lis + l, tmp, ((r - l) + 1) * sizeof(int));
-  print_array(lis, r - l + 1);
+  i = 0;
+  while (j <= m && k < len) { // Merge until one array runs out of data
+    if (buf[j] < buf[k]) {
+      lis[i] = buf[j];
+      j++;
+    } else {
+      lis[i] = buf[k];
+      k++;
+    }
+    i++;
+  }
+
+  // Any elements remaining in l?
+  while (j <= m) {
+    lis[i++] = buf[j++];
+  }
+  // Any elements remaining in m?
+  while (k < len) {
+    lis[i++] = buf[k++];
+  }
 }
+
 
 void merge(int *lis, int l, int r) {
   if (l < r) {
-    int m = l + (r - l) / 2;
+    int m = l + ((r - l) / 2);
+    printf("%d %d %d\n", l, m, r); 
 
     merge(lis, l, m);
     merge(lis, m + 1, r);
@@ -65,12 +83,12 @@ void merge(int *lis, int l, int r) {
 void main() {
   int test[] = {2, 4, 6, 1, 3, 5};
   int test_sorted[] = {1, 2, 3, 4, 5, 6};
-  int test2[] = {5, 3, 4, 2, 6, 1, -1, 8};
+  int test2[] = {2, 3, 4, 5, -1, 1, 6, 8};
   int test2_sorted[] = {-1, 1, 2, 3, 4, 5, 6, 8};
-  merge_sorted(test, 0, 5/2, 5);
+  merge_sorted(test, 0, 2, 5);
   test_eq(test, test_sorted, 6);
 
-  // TODO: merge(test2, 0, 7);
-  merge_sorted(test2, 0, 7/2, 7);
+  merge(test2, 0, 7);
+  //merge_sorted(test2, 0, 3, 7);
   test_eq(test2, test2_sorted, 8);
 }
