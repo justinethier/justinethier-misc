4af5c7406e47ae2f694f11fb15d20f132a04e38a
Justin Ethier <justin.ethier@gmail.com>
2022-04-29 14:47:28 -0700

    Fix bugs in example code

--- blog/kaprekars-constant-6174/code.js
+++ blog/kaprekars-constant-6174/code.js
@@ -3,16 +3,20 @@
 
 var num = 8614;
 
-iter = function(num) {
-  max = parseInt(num.toString().split('').sort().reverse().join(''));
-  min = parseInt(num.toString().split('').sort().join(''));
-  return max - min;
-}
+//iter = function(num) {
+//  max = parseInt(num.toString().padStart(4, '0').split('').sort().reverse().join(''));
+//  min = parseInt(num.toString().padStart(4, '0').split('').sort().join(''));
+//  return max - min;
+//}
 
 kaprekar = function(num) {
   while(num != 6174) {
-    num = iter(num);
+    max = parseInt(num.toString().padStart(4, '0').split('').sort().reverse().join(''));
+    min = parseInt(num.toString().padStart(4, '0').split('').sort().join(''));
+    num = max - min;
     console.log(num);
   }
+  return num;
 }
 
+kaprekar(1);
